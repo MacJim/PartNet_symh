42a4f57f348ec0a01768d5af309713c5771b7cd2
Jim Shi <shiyilin2008@gmail.com>
2020-04-12 14:58:29 -0700

    read_mat.py: This script now reads all mat files and print their contents.

--- read_mat.py
+++ read_mat.py
@@ -1,9 +1,24 @@
 # Reads MatLab `mat` file.
 
+import os
+
 import scipy.io
 
 
-filename = "part mesh indices/1.mat"
-content = scipy.io.loadmat(filename)
-print(content)
-print(content["cell_boxs_correspond_objSerialNumber"])
+def print_mat_file_content(filename: str):
+    content = scipy.io.loadmat(filename)
+    print(content)
+
+
+folders = ["ops", "part mesh indices", "boxes", "labels", "syms"]
+
+for folder in folders:
+    print("Folder ", folder, ":", sep="")
+    all_filenames = os.listdir(folder)
+    for filename in all_filenames:
+        if (filename.endswith(".mat")):
+            full_filename = os.path.join(folder, filename)
+            print_mat_file_content(full_filename)
+
+    print()
+        
